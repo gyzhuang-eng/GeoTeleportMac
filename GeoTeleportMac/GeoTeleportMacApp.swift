83331e9ae20cc088d97deaed676e7601930b221b
gyzhuang <gyzhuang-eng@users.noreply.github.com>
2025-12-19 15:43:13 +0400

    Initial Commit

---
+++ GeoTeleportMac/GeoTeleportMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoTeleportMacApp.swift
+//  GeoTeleportMac
+//
+//  Created by gyzhuang on 2025/12/19.
+//
+
+import SwiftUI
+
+@main
+struct GeoTeleportMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
